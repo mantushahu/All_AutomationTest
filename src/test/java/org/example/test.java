c470e5091350d387ea45ebe48c64be242b73ddda
Mantu <sunarmahendra88@gmail.com>
2021-03-26 12:03:58 -0500

    new line

--- src/test/java/org/example/test.java
+++ src/test/java/org/example/test.java
@@ -3,5 +3,6 @@ package org.example;
 public class test {
     public static void main(String[] args){
         System.out.println("hello mahendra");
+        System.out.println("man made horse");
     }
 }
